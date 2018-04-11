34e2a40a61599bf6d9058863572c63f735f9ef48
HuizhaoWang <913967754@qq.com>
2018-04-11 23:55:20 +0800

    宿舍修改，修改

--- mytest/chapter2_3.py
+++ mytest/chapter2_3.py
@@ -1,12 +1,25 @@
 import pandas as pd 
 
-
 #获取指定年数的文件信息
 def get_path(year):
     path_pre = "/home/whz/Documents/pydata-book/datasets/babynames/yob"
     return path_pre+str(year)+".txt"
 
-#读取txt的内容
-names1880 = pd.DataFrame(pd.read_csv(filepath_or_buffer=get_path(1880),sep=",",names=['name','sex','briths'])) 
-print(names1880.groupby(by="sex").briths.sum())                                                 
+columns = ['name','sex','briths']
 
+#读取txt的内容
+names1880 = pd.DataFrame(pd.read_csv("/home/whz/Documents/pydata-book/datasets/babynames/yob1880.txt",sep=",",names=columns))
+print(names1880.groupby(by="sex").briths.sum())   
+
+# 将每一个文件里的数据都读取出来，并且拼接在一起
+pieces = []
+for year in range(1880,2011):
+    path = get_path(year)
+    frame = pd.read_csv(path,names=columns)
+    frame['year'] = year
+    pieces.append(frame)
+
+#将所有的数据整合到单个DataFrame
+names = pd.concat(pieces,ignore_index=True)
+
+print(names)
